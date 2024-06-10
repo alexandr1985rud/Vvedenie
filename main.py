916a10de9ed4d944f8ec7ccb1bfc258f37a767cd
alexandr1985rud <alexandr.rud85@yandex.ru>
2024-06-10 22:10:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+print(' ')
+print('Задание 1')
+
+a = 'Травматолог'
+print(len(a))
+
+print(' ')
+print('Задание 2')
+
+first = 5
+second = 9
+summa = first + second
+diff = second - first
+print(summa, diff)
+
+print(' ')
+print('Задание 3')
+
+first = 9
+second = 15
+third = 6
+mean = (first + second + third) // 3
+print(mean)
+
+print(' ')
+print('Задание 4')
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+print(' ')
+print('Задание 5')
+
+a = 3
+b = 10
+c = 8
+f = (a * b) + (a * c)
+print(f ** 3 / 2)
+
